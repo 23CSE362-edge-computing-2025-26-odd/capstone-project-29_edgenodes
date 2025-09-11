cc0e4a7034d00b8f308cdbfac4e221718825164b
Viritha Daggubati <145008999+Viritha-D@users.noreply.github.com>
2025-09-12 01:50:29 +0530

    Entry Point

---
+++ main.py
@@ -0,0 +1,8 @@
+
+from simulator import TrafficSimulator
+
+CSV_PATH = r"C:\Users\daggu\Downloads\traffic.csv"  
+
+if __name__ == "__main__":
+    sim = TrafficSimulator(CSV_PATH)
+    sim.run_simulation()
